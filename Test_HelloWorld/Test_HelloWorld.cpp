44c85efde831df523377bd4352bc3bcdf88dab66
Jacob Cotham <JACotham@student.fullsail.edu>
2025-01-29 21:22:59 -0500

    Test project made and included class.h

---
+++ Test_HelloWorld/Test_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\HelloWorld\helloFunc.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestHelloWorld
+{
+	TEST_CLASS(TestHelloWorld)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
