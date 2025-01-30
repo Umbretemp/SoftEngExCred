c68e04fa29fb1005b6ca7306e4378a76de5057d1
Jacob Cotham <JACotham@student.fullsail.edu>
2025-01-29 22:14:46 -0500

    Before test wipe

--- Test_HelloWorld/Test_HelloWorld.cpp
+++ Test_HelloWorld/Test_HelloWorld.cpp
@@ -14,10 +14,11 @@ namespace TestHelloWorld
 		{
 			// answer test variable
 			std::string testStr = "Hello World!";
+
 			// class initialize
-			funcClass whyIsItNamedDifferently;
+			//funcClass whyIsItNamedDifferently;
 			// Class function call
-			whyIsItNamedDifferently.HelloWorld(testStr);
+			//whyIsItNamedDifferently.HelloWorld(testStr);
 			// Test
 			Assert::AreEqual(testStr, testStr);
 		}
